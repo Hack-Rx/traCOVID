3998ae841e0e9fa90fa05600ac91bed7e758b6fe
tanmaya0102 <tanmaya0102@gmail.com>
2020-06-13 13:05:50 +0530

    Added Insurance CHROME URI Intent

--- app/src/main/java/com/tanmaya/tracovid/InsureActivity.java
+++ app/src/main/java/com/tanmaya/tracovid/InsureActivity.java
@@ -1,14 +1,18 @@
 package com.tanmaya.tracovid;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InsureActivity extends AppCompatActivity {
-    Button NavHome,NavCB,NavDiag;
+    Button NavHome,NavCB,NavDiag,OC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,7 @@ public class InsureActivity extends AppCompatActivity {
         NavHome=findViewById(R.id.button7);
         NavCB=findViewById(R.id.button8);
         NavDiag=findViewById(R.id.button10);
+        OC=findViewById(R.id.button3);
         NavCB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,5 +45,28 @@ public class InsureActivity extends AppCompatActivity {
                 finish();
             }
         });
+        OC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrlInChrome();
+            }
+        });
+
+    }
+    void openUrlInChrome() {
+        try {
+            Intent i = new Intent("android.intent.action.MAIN");
+            i.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+            i.addCategory("android.intent.category.LAUNCHER");
+            i.setData(Uri.parse("https://www.bajajfinserv.in/health-insurance"));
+            startActivity(i);
+        }
+        catch(ActivityNotFoundException e) {
+// Chrome is probably not installed
+// OR not selected as default browser OR if no Browser is selected as default browser
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bajajfinserv.in/health-insurance"));
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+        }
     }
 }
