e0348a350cc1a260454088213e54ab3959ce4f59
tanmaya0102 <tanmaya0102@gmail.com>
2020-06-13 15:51:13 +0530

    Chat Fuctionality is Added

--- app/src/main/java/com/tanmaya/tracovid/ConsultActivity.java
+++ app/src/main/java/com/tanmaya/tracovid/ConsultActivity.java
@@ -7,9 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ConsultActivity extends AppCompatActivity {
     Button NavHome,NavDiag,NavIns,NavCB;
+    private DatabaseReference myDatabase;
+            EditText myEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +61,29 @@ public class ConsultActivity extends AppCompatActivity {
                 finish();
             }
         });
-        
+        myDatabase = FirebaseDatabase.getInstance().getReference("Message");
+
+        final TextView myText = findViewById(R.id.textbox);
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                myText.setText(dataSnapshot.getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                myText.setText("CANCELLED");
+
+            }
+        });
     }
-}
+    public void sendMessage(View view){
+        myEditText = findViewById(R.id.editText);
+
+        myDatabase.child(Long.toString(System.currentTimeMillis())).setValue(myEditText.getText().toString());
+        myEditText.setText("\n");
+    }
+
+    }
